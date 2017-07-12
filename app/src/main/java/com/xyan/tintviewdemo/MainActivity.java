4144e914f4dbc851aa344923e5b305f3dc54628f
camila <camila@camiladeiMac.lan>
2017-07-12 23:45:57 +0800

    add image view with wrong anim

---
+++ app/src/main/java/com/xyan/tintviewdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.xyan.tintviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img = (ImageView) findViewById(R.id.img_view);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("cxq", "onClick: ");
+            }
+        });
+    }
+}
---
+++ tintview/src/main/java/com/xyan/tintview/TintImageView.java
@@ -0,0 +1,109 @@
+package com.xyan.tintview;
+
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+/**
+ * Created by chenxueqing on 2017/7/12.
+ */
+
+public class TintImageView extends AppCompatImageView {
+
+    private final String TAG = "TintImageView";
+
+    @ColorInt
+    private int normalColor;
+    @ColorInt
+    private int pressedColor;
+    @ColorInt
+    private int currentColor;
+    private boolean needAnim;
+    private long animMills;
+
+    private ValueAnimator animator;
+    private ArgbEvaluator argbEvaluator;
+
+    public TintImageView(Context context) {
+        this(context, null);
+    }
+
+    public TintImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.TintImageView);
+        normalColor = ta.getColor(R.styleable.TintImageView_tiv_normal_color, Color.TRANSPARENT);
+        pressedColor = ta.getColor(R.styleable.TintImageView_tiv_pressed_color, ContextCompat.getColor(getContext(), R.color.default_pressed_color));
+        currentColor = normalColor;
+        needAnim = ta.getBoolean(R.styleable.TintImageView_tiv_need_anim, false);
+        animMills = ta.getInt(R.styleable.TintImageView_tiv_anim_time, 300);
+        ta.recycle();
+
+        setColorFilter(normalColor);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: " + event.getAction());
+        if (!isClickable()) {
+            return super.onTouchEvent(event);
+        }
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                handleTouchAction(true);
+                break;
+            case MotionEvent.ACTION_UP:
+                handleTouchAction(false);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                handleTouchAction(false);
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void initAnimator() {
+        //second protect
+        if (animator == null) {
+            animator = new ValueAnimator();
+            argbEvaluator = new ArgbEvaluator();
+
+            animator.setDuration(animMills);
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    currentColor = (Integer) animation.getAnimatedValue();
+                    setColorFilter(currentColor);
+                }
+            });
+        }
+    }
+
+    private void handleTouchAction(boolean isPress) {
+        if (needAnim) {
+            if (animator == null) {
+                initAnimator();
+            }
+            if (animator.isRunning()) {
+                animator.cancel();
+            }
+            animator.setIntValues(currentColor, isPress ? pressedColor : normalColor);
+            animator.setEvaluator(argbEvaluator);
+            animator.start();
+        } else {
+            setColorFilter(isPress ? pressedColor : normalColor);
+        }
+    }
+}
